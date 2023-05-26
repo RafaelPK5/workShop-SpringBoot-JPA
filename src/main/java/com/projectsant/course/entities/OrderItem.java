b255441f398bba2a7b2df3eee187e4c41fa3848d
RafaelPK5 <contato.rafaelsantanak5@gmail.com>
2023-05-26 16:45:20 -0300

    Subtotal & Total methods

--- src/main/java/com/projectsant/course/entities/OrderItem.java
+++ src/main/java/com/projectsant/course/entities/OrderItem.java
@@ -39,7 +39,6 @@ public class OrderItem implements Serializable{
 		id.setOrder(order);
 	}
 	
-	
 	public Product getProduct() {
 		return id.getProduct();	
 	}
@@ -63,7 +62,11 @@ public class OrderItem implements Serializable{
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
