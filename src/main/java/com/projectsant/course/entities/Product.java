d47b27f27797a5799fffa918bc3d14f7e077a30d
RafaelPK5 <contato.rafaelsantanak5@gmail.com>
2023-05-29 16:08:14 -0300

    finalizing exceptions

--- src/main/java/com/projectsant/course/entities/Product.java
+++ src/main/java/com/projectsant/course/entities/Product.java
@@ -18,7 +18,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
+@Table(name = "tb_product")
 public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
---
+++ src/main/java/com/projectsant/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.projectsant.course.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.projectsant.course.services.exceptions.DataBaseException;
+import com.projectsant.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Resource net found!";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request){
+		String error = "Database error!";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
